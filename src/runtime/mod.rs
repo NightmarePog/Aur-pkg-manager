a06ed45384a63fb38691f606a8db6ee121f13b31
Nightmare Pog <44711407+NightmarePog@users.noreply.github.com>
2026-07-26 14:46:21 +0200

    feat: add runtime

---
+++ src/runtime/mod.rs
@@ -0,0 +1,13 @@
+pub fn build(package: &str) -> anyhow::Result<()> {
+    tracing::info!("Building {package}");
+
+    // TODO
+    Ok(())
+}
+
+pub fn launch(package: &str) -> anyhow::Result<()> {
+    tracing::info!("Launching {package} in sandbox");
+
+    // TODO
+    Ok(())
+}
