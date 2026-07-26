1623f6feaec9936f7e7fd40a7f626ff1e827dc80
Nightmare Pog <44711407+NightmarePog@users.noreply.github.com>
2026-07-26 14:46:21 +0200

    feat: wire up cli in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+mod aur;
+mod cli;
+mod logging;
+mod runtime;
+mod store;
+
+use clap::Parser;
+use cli::Cli;
+
+fn main() -> anyhow::Result<()> {
+    logging::init_logging();
+
+    let cli = Cli::parse();
+
+    match cli.command {
+        cli::Command::Install { package } => {
+            cli::install::install(package)?;
+        }
+        cli::Command::Run { package } => {
+            cli::run::run(package)?;
+        }
+        cli::Command::Remove { package } => {
+            cli::remove::remove(package)?;
+        }
+    }
+
+    Ok(())
 }
